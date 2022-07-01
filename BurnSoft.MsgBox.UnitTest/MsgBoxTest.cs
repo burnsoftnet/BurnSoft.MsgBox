680a87d3108b6417e561c6a7e1e9007010151294
BurnSoft <bs.github@burnsoft.net>
2022-07-01 08:08:41 -0400

    #4 Added unit test and initial test from app

---
+++ BurnSoft.MsgBox.UnitTest/MsgBoxTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace BurnSoft.MsgBox.UnitTest
+{
+    [TestClass]
+    public class MsgBoxTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            MsgClass obj = new MsgClass();
+            obj.DoMessage("It has been 7 days since your last backup.", MgboxStyle.Inf_OK, MgBtnStyle.mb_Exclamantion,
+                "Last Backup Notice", AutoCloseTimer.ac_0Sec, true, "Warning");
+        }
+    }
+}
---
+++ BurnSoft.MsgBox.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BurnSoft.MsgBox.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BurnSoft.MsgBox.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a2aeaccd-5128-4645-9b9c-f301f27918a9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
