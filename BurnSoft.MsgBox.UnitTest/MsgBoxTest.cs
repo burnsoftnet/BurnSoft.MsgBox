ba0623c28bebb7d631953de7ef1f92e88e262dee
BurnSoft <bs.github@burnsoft.net>
2022-07-01 08:15:16 -0400

    #4 finished unit test

--- BurnSoft.MsgBox.UnitTest/MsgBoxTest.cs
+++ BurnSoft.MsgBox.UnitTest/MsgBoxTest.cs
@@ -6,12 +6,169 @@ namespace BurnSoft.MsgBox.UnitTest
     [TestClass]
     public class MsgBoxTest
     {
-        [TestMethod]
-        public void TestMethod1()
+        /// <summary>
+        /// Gets or sets the test context.
+        /// </summary>
+        /// <value>The test context.</value>
+        public TestContext TestContext { get; set; }
+        /// <summary>
+        /// The object
+        /// </summary>
+        MsgClass obj;
+        [TestInitialize]
+        public void Init()
         {
-            MsgClass obj = new MsgClass();
-            obj.DoMessage("It has been 7 days since your last backup.", MgboxStyle.Inf_OK, MgBtnStyle.mb_Exclamantion,
-                "Last Backup Notice", AutoCloseTimer.ac_0Sec, true, "Warning");
+            obj = new MsgClass();
+        }
+
+        [TestMethod]
+        public void WarningOkTest()
+        {
+            try
+            {
+                obj.DoMessage("It has been 7 days since your last backup.", MgboxStyle.Inf_OK, MgBtnStyle.mb_Exclamantion,
+                    "Last Backup Notice", AutoCloseTimer.ac_0Sec, true, "Warning");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        [TestMethod]
+        public void CriticalOkTest()
+        {
+            try
+            {
+                obj.DoMessage("It has been 7 days since your last backup.", MgboxStyle.Inf_OK, MgBtnStyle.mb_Critical,
+                    "Last Backup Notice", AutoCloseTimer.ac_0Sec, true, "Warning");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        [TestMethod]
+        public void QuestionOkTest()
+        {
+            try
+            {
+                obj.DoMessage("It has been 7 days since your last backup.", MgboxStyle.Inf_OK, MgBtnStyle.mb_Question,
+                    "Last Backup Notice", AutoCloseTimer.ac_0Sec, true, "Warning");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        [TestMethod]
+        public void OkTest()
+        {
+            try
+            {
+                obj.DoMessage("It has been 7 days since your last backup.", MgboxStyle.Inf_OK, MgBtnStyle.mb_Information,
+                    "Last Backup Notice", AutoCloseTimer.ac_0Sec, true, "Info");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        [TestMethod]
+        public void OkAudo10SecondsTest()
+        {
+            try
+            {
+                obj.DoMessage("It has been 7 days since your last backup.", MgboxStyle.Inf_OK, MgBtnStyle.mb_Information,
+                    "Last Backup Notice", AutoCloseTimer.ac_10Secs, true, "Info");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        [TestMethod]
+        public void OkCancelTest()
+        {
+            try
+            {
+                obj.DoMessage("It has been 7 days since your last backup.", MgboxStyle.Inf_OKCancel, MgBtnStyle.mb_Information,
+                    "Last Backup Notice", AutoCloseTimer.ac_0Sec, true, "Info");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        [TestMethod]
+        public void RetryTest()
+        {
+            try
+            {
+                obj.DoMessage("It has been 7 days since your last backup.", MgboxStyle.Inf_Retry, MgBtnStyle.mb_Information,
+                    "Last Backup Notice", AutoCloseTimer.ac_0Sec, true, "Info");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        [TestMethod]
+        public void RetryCancelTest()
+        {
+            try
+            {
+                obj.DoMessage("It has been 7 days since your last backup.", MgboxStyle.Inf_RetryCancel, MgBtnStyle.mb_Information,
+                    "Last Backup Notice", AutoCloseTimer.ac_0Sec, true, "Info");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        [TestMethod]
+        public void YesNoTest()
+        {
+            try
+            {
+                obj.DoMessage("It has been 7 days since your last backup.", MgboxStyle.Inf_YesNo, MgBtnStyle.mb_Information,
+                    "Last Backup Notice", AutoCloseTimer.ac_0Sec, true, "Info");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        [TestMethod]
+        public void YesNoCancelTest()
+        {
+            try
+            {
+                obj.DoMessage("It has been 7 days since your last backup.", MgboxStyle.Inf_YesNoCancel, MgBtnStyle.mb_Information,
+                    "Last Backup Notice", AutoCloseTimer.ac_0Sec, true, "Info");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
         }
     }
 }
